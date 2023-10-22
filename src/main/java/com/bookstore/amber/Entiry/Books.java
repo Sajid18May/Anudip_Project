c20a938a599d3ff27f8caeba0c08e2b906f03f63
Sajid18May <sajid1852002@gmail.com>
2023-10-22 12:19:58 +0530

    ER mapping added

--- src/main/java/com/bookstore/amber/Entiry/Books.java
+++ src/main/java/com/bookstore/amber/Entiry/Books.java
@@ -1,11 +1,15 @@
 package com.bookstore.amber.Entiry;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -20,4 +24,8 @@ private String genre;
 private double price;
 private Date published_date;
 private int stock_quantity;
+
+@OneToMany(cascade = CascadeType.ALL)
+@JoinColumn(name="book_id",referencedColumnName = "book_id")
+private List<Orders> orders;
 }
--- src/main/java/com/bookstore/amber/Entiry/Customers.java
+++ src/main/java/com/bookstore/amber/Entiry/Customers.java
@@ -1,9 +1,14 @@
 package com.bookstore.amber.Entiry;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -18,4 +23,8 @@ private String email;
 private String password ;
 private String address;
 private String phone_number;
+
+@OneToMany(cascade = CascadeType.ALL)
+@JoinColumn(name="customer_id",referencedColumnName = "customer_id")
+private List<Orders> orders;
 }
