d150f9848c3862b1f7055a5f8bf8679db71dbd66
Sajid18May <sajid1852002@gmail.com>
2023-12-11 10:43:27 +0530

    order date added in the orders entity

--- src/main/java/com/bookstore/amber/Entiry/Orders.java
+++ src/main/java/com/bookstore/amber/Entiry/Orders.java
@@ -1,5 +1,9 @@
 package com.bookstore.amber.Entiry;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +18,10 @@ public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int order_id;
-    // private int customer_id ;
-    // private int book_id ;
     private int totalcount;
     private double totalprice;
+    @DateTimeFormat(pattern = "yy-MM-dd")
+    private Date order_date;
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customers customers;
